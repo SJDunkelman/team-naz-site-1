fe66739857fa8324ae10e2c45e5e2d30a28a5839
Simon Dunkelman <sd9518@ic.ac.uk>
2023-07-28 17:19:59 +0100

    updated social card and site desc.

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 
 export const TWITTER_URL = "https://twitter.com/"
-export const SITE_NAME = "PATHIO"
-export const SITE_DESCRIPTION = ""
+export const SITE_TITLE = "Pathio"
+export const SITE_DESCRIPTION = "Navigate Life's Crossroads with Confidence"
 export const MAILTO_LINK = "mailto:test@example.com"
