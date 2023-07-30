3298519a11e89c63baa062b45d6159dfbd898f5d
Simon Dunkelman <sd9518@ic.ac.uk>
2023-07-30 13:22:32 +0100

    removed subtext in how it works

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -8,15 +8,15 @@ export default function HowItWorks() {
     return (
         <section className='py-24 bg-white overflow-hidden' id='how-it-works'>
           <div className='container px-4 mx-auto'>
-            <div className='max-w-xl text-center mb-20'>
-              <h2 className='font-heading mb-5 text-6xl tracking-tighter'>
+            <div className='max-w-xl w-full flex items-center mx-auto text-center'>
+              <h2 className='text-center font-heading mb-5 text-6xl tracking-tighter'>
               Embark on Your Success Journey with Pathio
               </h2>
                 {/* <p className='text-xl tracking-tight'>
                 Navigate Life's Crossroads with Confidence. Pathio Empowers Creative Spirits Like You to Triumph Over Challenges and Transitions, Leading to Unmatched Success and Fulfillment.
                 </p> */}
             </div>
-            <div className='flex flex-col md:flex-row items-center justify-center mb-14'>
+            <div className='flex flex-col md:flex-row items-center justify-center mb-6'>
               <div className='w-full md:w-1/3 p-7 flex justify-center'>
                 <div className='max-w-xs flex flex-col text-center items-center justify-center'>
                   <img
@@ -27,9 +27,9 @@ export default function HowItWorks() {
                   <h3 className='mb-4 text-xl font-semibold tracking-tight'>
                   Figure out what you want to do next
                   </h3>
-                  <p className='text-gray-600 tracking-tight'>
+                  {/* <p className='text-gray-600 tracking-tight'>
                   Experience the Precision of Being Paired with the Ideal Coach or Mentor, Catering Specifically to Your Aspirations and Challenges.
-                  </p>
+                  </p> */}
                 </div>
               </div>
               <div className='w-full md:w-1/3 p-7 flex justify-center'>
@@ -42,9 +42,9 @@ export default function HowItWorks() {
                   <h3 className='mb-4 text-xl font-semibold tracking-tight'>
                   Grow into a new role with confidence
                   </h3>
-                  <p className='text-gray-600 tracking-tight'>
+                  {/* <p className='text-gray-600 tracking-tight'>
                   Dive into a Vast Ocean of Personalized Content, Meticulously Curated to Propel Your Personal and Professional Evolution.
-                  </p>
+                  </p> */}
                 </div>
               </div>
               <div className='w-full md:w-1/3 p-7 flex justify-center'>
@@ -57,9 +57,9 @@ export default function HowItWorks() {
                   <h3 className='mb-4 text-xl font-semibold tracking-tight'>
                   Switch jobs or careers successfully
                   </h3>
-                  <p className='text-gray-600 tracking-tight'>
+                  {/* <p className='text-gray-600 tracking-tight'>
                   Leverage Pathio's Expertise to Steer Through Life's Crossroads with Certainty, Empowering You to Make Confident Decisions.
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </div>
