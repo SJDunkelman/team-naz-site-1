491c3efe9a5af649b3bfdd6c56ed665d5c0e2bbd
Simon Dunkelman <sd9518@ic.ac.uk>
2023-07-30 17:36:46 +0100

    reverted button text to refined copy

--- src/components/SurveyButton.jsx
+++ src/components/SurveyButton.jsx
@@ -10,7 +10,7 @@ export default function SurveyButton() {
             id='y6r8249g'
             className='px-4 py-2 rounded-xl text-lg font-medium bg-indigo-500 hover:bg-indigo-600 transition ease-in-out duration-200 text-white w-min whitespace-nowrap'
           >
-            Get Started Now
+            Match with your coach
           </PopupButton>
         </p>
     )
